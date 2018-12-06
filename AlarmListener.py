a676fe61e86af8e581ffda2688433901037baa98
danbrick92 <danbrickner@hotmail.com>
2018-12-06 17:05:16 +0000

    Adding structure

--- AlarmListener.py
+++ AlarmListener.py
@@ -15,12 +15,9 @@ import math
 import pyaudio
 import wave
 import sys
+import datetime
 from pyAudioAnalysis import audioBasicIO
 from pyAudioAnalysis import audioFeatureExtraction
-import matplotlib.pyplot as plt
-
-
-ALARM_COUNT = 0
 
 CHUNK = 4096
 FORMAT = pyaudio.paInt16
@@ -31,6 +28,7 @@ WAVE_OUTPUT_FILENAME = "output.wav"
 
 # This function records audio from the microphone
 def record_audio():
+    print("Will record " + str(RECORD_SECONDS) + " of audio")
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT,
                 channels=CHANNELS,
@@ -49,6 +47,7 @@ def record_audio():
     
 # Saves the recorded audio to a wav file
 def save_wav():
+    print("Saving audio to " + str(WAVE_OUTPUT_FILENAME))
     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
@@ -57,34 +56,44 @@ def save_wav():
     wf.close()
 
 # Analyze dB to match alarm sound levels
-def analysis():
+def analysis(alarm):
+    print("Analyzing audio for alarm levels of loudness")
     [Fs, x] = audioBasicIO.readAudioFile("output.wav");
     F, f_names = audioFeatureExtraction.stFeatureExtraction(x, Fs, 1.0000*Fs, 0.025*Fs);
     averageEnergy = sum(F[1,:])/len(F[1,:])
-    if averageEnergy >= 1.5:
-        ALARM_COUNT +=1
+    if averageEnergy >= .15:
+        alarm["count"] += 1
+        print("Alarm loudness reached. Count is " + str(alarm["count"]))
+    return alarm
 
 # Triggers alarm based on ALARM COUNT
-def check_trigger_alarm():
-    if ALARM_COUNT >= 3:
-        trigger_alarm()
+def check_trigger_alarm(alarm):
+    print("Checking if alarm state is reached.")
+    if alarm["count"] >= 3:
+        print "Triggering alarm..."
+        alarm = trigger_alarm(alarm)
+    return alarm
         
 # Logic for what alarm trigger does
-def trigger_alarm():
-    ALARM_COUNT = 0
+def trigger_alarm(alarm):
     send_sms("The security system alarm has been triggered")
+    alarm["count"] = 0
+    print("Waiting for one hour")
     time.sleep(3600) # Sleep for an hour - don't want to be bombarded with texts
+    return alarm
     
 # Sends a text message to numbers from data file
 def send_sms(message):
     return 0
 
 def main():
+    print("Starting loop")
+    alarm = { "count" : 0, "last_triggered" : datetime.datetime.now()}
     while True:
         #record_audio()
         #save_wav()
-        analysis()
-        check_trigger_alarm()
+        alarm = analysis(alarm)
+        alarm = check_trigger_alarm(alarm)
         time.sleep(2)
     
 if __name__ == "__main__":
